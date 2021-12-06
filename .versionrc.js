e9dfb18d552c4ca52b6a942eb3f23a1678654ff6
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-12-06 14:14:57 +0700

    fix: standard-version configuration ignored package.json

--- .versionrc.js
+++ .versionrc.js
@@ -2,6 +2,7 @@ const defaultStandardVersion = require("@dnb-org/standard-version-config");
 
 const localStandardVersion = {
   bumpFiles: [
+    ...defaultStandardVersion.bumpFiles,
     { filename: "data/dnb/netlification/version.json", type: "json" },
   ],
 };
